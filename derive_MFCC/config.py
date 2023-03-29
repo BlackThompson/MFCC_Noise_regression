4fab92c92c659b56f38dc31f1641603d7827cbdf
Black <1587754061@qq.com>
2023-03-29 08:19:37 +0800

    add automl

--- derive_MFCC/config.py
+++ derive_MFCC/config.py
@@ -6,4 +6,14 @@
 
 columns_mean = [f'{i}_mean' for i in range(1, 21)]
 columns_std = [f'{i}_std' for i in range(1, 21)]
+columns_25_percentile = [f'{i}_25_per' for i in range(1, 21)]
+columns_75_percentile = [f'{i}_75_per' for i in range(1, 21)]
+# 峰度
+columns_kurtosis = [f'{i}_kurtosis' for i in range(1, 21)]
+# 偏度
+columns_skew = [f'{i}_skew' for i in range(1, 21)]
+
 columns = ['category', 'file_name', 'score'] + columns_mean + columns_std
+
+columns_new_1 = ['category', 'file_name', 'score'] + columns_mean + columns_25_percentile + \
+                columns_75_percentile + columns_kurtosis + columns_skew
